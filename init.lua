83d8052a8d2008c0a141d461a055629852ee84ba
HEARN, DAVID <s-dahearn@lwsd.org>
2018-07-05 01:41:50 -0700

    Revert "Adding abm, depends, intt, mapgen, nodes, and textures"
    This reverts commit 885c604d7c93514200f07f4829724f8af23db855.

--- init.lua
+++
@@ -1,5 +0,0 @@
---include
-
-dofile(minetest.get_modpath('magic_grenade') .. '/nodes.lua')
-dofile(minetest.get_modpath('magic_grenade') .. '/mapgen.lua')
-dofile(minetest.get_modpath('magic_grenade') .. '/abm.lua')
